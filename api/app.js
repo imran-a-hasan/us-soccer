9e49b52c3cdc4dbdf9a50afa8c3d75119b53c952
imran-a-hasan <imran.hasan@gmail.com>
2020-09-19 17:36:34 -0700

    more extra files removed

--- api/app.js
+++ api/app.js
@@ -3,11 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var testAPIRouter = require("./routes/testAPI");
 var cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var getSchedule = require('./routes/getSchedule');
 var getResults = require('./routes/getResults');
 
@@ -23,9 +20,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/api', testAPIRouter)
 app.get('/schedule', getSchedule);
 app.get('/results', getResults)
 
