238787f54537b0c47122e5f1fb0bc3faa0064b20
imran-a-hasan <imran.hasan@gmail.com>
2020-11-09 20:43:41 -0800

    warning fixes

--- frontend/src/EmojiKey.js
+++ frontend/src/EmojiKey.js
@@ -4,10 +4,10 @@ function EmojiKey() {
     return (
         <div className='emoji-key'>
             <span className='minutes-key'></span> - minutes played
-            <div>🪑 - bench</div>
-            <div>❌ - not in squad</div>
-            <div>⚽ - goal</div>
-            <div>🅰 - assist</div>
+            <div><span role='img' aria-label='bench'>&#x1FA91;</span> - bench</div> 
+            <div><span role='img' aria-label='not in squad'>&#x274C;</span> - not in squad</div>
+            <div><span role='img' aria-label='goal'>&#9917;</span> - goal</div>
+            <div><span role='img' aria-label='assist'>&#x1F170;</span> - assist</div>
         </div>
     );
 }
