d883051c42cb9069dffe0c8c3b3bdbcc09918e73
imran-a-hasan <imran.hasan@gmail.com>
2020-11-25 18:17:32 -0800

    small cleanup

--- api/app.js
+++ api/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/schedule', getSchedule);
-app.get('/results', getResults)
+app.get('/results', getResults);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
